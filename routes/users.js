7d635393659643ab8295e66ff90019746292dc3c
Kopano <Kopano@securitybis.co.za>
2025-05-21 18:51:10 +0200

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send("User list")
+});
+
+router.get("/new", (req, res) => {
+    res.send("New user form")
+});
+
+router.post("/", (req, res) => {
+    res.send("Create user")
+})
+
+// url checks each route, make sure static routes come before dynamic routes such as /:id because /new will get mistaken as an id
+
+const users = [{name: "Kopano"}, {name: "David"}];
+router.param("id", (req, res, next, id) => { // middleware that runs any time a id param is found
+    req.user = users[id].name;
+    next(); //with out this function the rest of the code won't run
+})
+
+router
+.route("/:id")
+.get((req, res) => {
+    res.send(`Send info related to user: ${req.params.id} ${req.user}`)
+})
+.post((req, res) => {
+    res.send(`Update info related to user: ${req.params.id}`)
+})
+.delete((req, res) => {
+    res.send(`Update info related to user: ${req.params.id}`)
+})
+module.exports = router
---
+++ server.js
@@ -0,0 +1,20 @@
+
+const express = require("express"); // require the library we installed
+const app = express(); // call express function
+
+app.set("view engine", "ejs"); //set up view engine to pass info to html file and to load html from the server
+
+app.get("/", (req, res) => {
+    console.log("Here");
+    //res.send("Hi");
+    // res.status(500).send("Oops error on our end"); // send normal text info, good for testing
+    // res.status(500).json({message: "Oops error on our end"}); // send json info, good for APIs
+    // res.download("server.js"); // send files to client
+    res.render("index", {text: "world"}); //send html file to client
+})
+
+// route info related to users to the user.js file
+const userRouter = require("./routes/users");
+app.use("/users", userRouter)
+
+app.listen(3000); // port number used for the app
